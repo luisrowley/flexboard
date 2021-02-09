537cf5850be4f3b676aba47a7665df41ad049378
Luis De Benito <luissebastian.debenito@pfsgroup.es>
2021-02-09 13:50:50 +0100

    added layout configuration input property as json.

--- projects/ngx-flexboard-app/src/app/app.component.ts
+++ projects/ngx-flexboard-app/src/app/app.component.ts
@@ -9,5 +9,160 @@ export class AppComponent {
   title = 'ngx-flexboard-app';
 
   // dashboard conf
-  public layout = [];
+  public layout = {
+    "title": "uneven",
+    "rows": [
+      {
+        "rowspan": 1,
+        "tiles": [
+          {
+            "colspan": 4,
+            "size": "lg",
+            "isAuto": false,
+            "component": "TileComponent"
+          },
+          {
+            "colspan": 4,
+            "size": "md",
+            "isAuto": false,
+            "component": "TileComponent"
+          },
+          {
+            "colspan": 4,
+            "size": "md",
+            "isAuto": false,
+            "component": "TileComponent"
+          }
+        ]
+      },
+      {
+        "rowspan": 1,
+        "tiles": [
+          {
+            "colspan": 5,
+            "size": "md",
+            "isAuto": false,
+            "component": "TileComponent"
+          },
+          {
+            "colspan": 4,
+            "size": "md",
+            "isAuto": false,
+            "component": "TileComponent"
+          },
+          {
+            "colspan": 3,
+            "size": "md",
+            "isAuto": false,
+            "component": "TileComponent"
+          }
+        ]
+      },
+      {
+        "rowspan": 1,
+        "tiles": [
+          {
+            "colspan": 3,
+            "size": "md",
+            "isAuto": false,
+            "component": "TileComponent"
+          },
+          {
+            "colspan": 3,
+            "size": "md",
+            "isAuto": false,
+            "component": "TileComponent"
+          },
+          {
+            "colspan": 6,
+            "size": "md",
+            "isAuto": false,
+            "component": "TileComponent"
+          }
+        ]
+      }
+    ]
+  };
+
+  public layoutNested = {
+    "title": "nested",
+    "rows": [
+      {
+        "rowspan": 1,
+        "tiles": [
+          {
+            "colspan": 4,
+            "size": "md",
+            "isAuto": false,
+            "component": "TileComponent"
+          },
+          {
+            "colspan": 2,
+            "size": "lg",
+            "isAuto": false,
+            "component": "TileComponent",
+            "rows": [
+              {
+                "rowspan": 1,
+                "tiles": [
+                  {
+                    "colspan": 12,
+                    "size": "md",
+                    "isAuto": false,
+                    "component": "TileComponent"
+                  }
+                ]
+              },
+              {
+                "rowspan": 1,
+                "tiles": [
+                  {
+                    "colspan": 12,
+                    "size": "md",
+                    "isAuto": false,
+                    "component": "TileComponent"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "colspan": 4,
+            "size": "md",
+            "isAuto": false,
+            "component": "TileComponent"
+          },
+          {
+            "colspan": 2,
+            "size": "md",
+            "isAuto": false,
+            "component": "TileComponent"
+          }
+        ]
+      },
+      {
+        "rowspan": 1,
+        "tiles": [
+          {
+            "colspan": 4,
+            "size": "md",
+            "isAuto": false,
+            "component": "TileComponent"
+          },
+          {
+            "colspan": 4,
+            "size": "md",
+            "isAuto": false,
+            "component": "TileComponent"
+          },
+          {
+            "colspan": 4,
+            "size": "md",
+            "isAuto": false,
+            "component": "TileComponent"
+          }
+        ]
+      }
+    ]
+  }
 }
